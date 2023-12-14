d1d5f9d97eba0447f65360a701c11cddde076d8f
AKSHAY SHIRSAT <arsofficial28@gmail.com>
2023-12-14 15:13:01 +0530

    update changes

---
+++ new-project/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <h2>Technokeens</h2>
+<storeResorces :resources="storedResorcesData"/>
+</template>
+
+<script>
+import storeResorces from './components/learningComp/storeResorces.vue';
+
+export default {
+  components: {
+    storeResorces,
+},
+  data() {
+    return {
+      storedResorcesData: [
+        {
+          id: "official-guide",
+          title: "official guide",
+          descriptions: "the official vue js documantion",
+          link: "http://vuejs.org",
+        },
+        {
+          id: "google-guide",
+          title: "Google guide",
+          descriptions: "the official google documantion",
+          link: "http://google.org",
+        },
+      ],
+    };
+  },
+  methods: {},
+};
+</script>
+
+<style>
+*{
+  box-sizing: border-box;
+}
+body{
+  margin: 0%;
+}
+
+
+</style>
---
+++ new-project/src/components/learningComp/StoreResorces.vue
@@ -0,0 +1,25 @@
+<template>
+    test
+<ul>
+<LearningResorces v-for="res in resources"
+      :key="res.id"
+      :title="res.title"
+      :descriptions="res.descriptions"
+      :link="res.link" />
+  </ul>
+</template>
+
+<script>
+import LearningResorces from "./LearningResorces.vue"
+export default{
+components:{
+    LearningResorces
+},
+props:['resources'],
+}
+
+</script>
+
+<style>
+
+</style>
